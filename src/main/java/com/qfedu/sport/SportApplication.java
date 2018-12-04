4b52aa22c4e04d7411c571f607e6ad160f1c9796
w514071646 <gaoyue123>
2018-12-04 17:37:08 +0800

    Initial commit

---
+++ src/main/java/com/qfedu/sport/SportApplication.java
@@ -0,0 +1,12 @@
+package com.qfedu.sport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportApplication.class, args);
+    }
+}
