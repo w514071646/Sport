2296f6ceb2c1b3630c4336497c52c15ebe965f80
w514071646 <gaoyue123>
2018-12-07 21:42:19 +0800

    高悦12.7日第一次提交

--- src/main/java/com/qfedu/sport/configuration/BaseConfiguration.java
+++ src/main/java/com/qfedu/sport/configuration/BaseConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-import javax.servlet.MultipartConfigElement;
 
 /**
  * @author gfc
